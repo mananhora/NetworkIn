3c5a02763255038ccb9633e0d1792b661d4102c1
Manan Hora <horamanan@gmail.com>
2015-11-02 22:57:49 -0500

    Added code to create a new user

--- Server/app.js
+++ Server/app.js
@@ -16,3 +16,23 @@ var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.user(bodyParser.urlencoded({ extended: true}));
+
+
+// CREATE a new user
+app.post('/users', function (req, res) {
+  var postBody = req.body;
+  var myName = postBody.name;
+
+  // must have a name!
+  if (!myName) {
+    res.send('ERROR');
+    return; // return early!
+  }
+
+  // check if user's name is already in database; if so, send an error
+
+  // otherwise add the user to the database by pushing (appending)
+  // postBody to the fakeDatabase list
+
+  res.send('OK');
+});
