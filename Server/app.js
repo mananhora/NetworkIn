d94b6e75128ed4c42fd99c953b7d79ae3272f049
Nina Bose <nbose2@u.rochester.edu>
2015-11-08 10:30:36 -0500

    Functionality for invalid password and already existing username.

--- Server/app.js
+++ Server/app.js
@@ -1,6 +1,6 @@
 /*
- * STUDY ROOMS
- * Developers: Manan Hora, Nina Bose
+ * PersonalCRM
+ * Developers: Nina Bose, Manan Hora
  * October 22nd, 2015
  */
 
@@ -22,13 +22,15 @@ client.connect();
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 
 app.use(express.static('../client'));
 
 
-app.get('/users/*', function (req, res) {
+app.get('/users/*', function(req, res) {
   console.log('hello');
   res.send('{}');
   // res.render('index', {});
@@ -36,7 +38,7 @@ app.get('/users/*', function (req, res) {
 
 
 // CREATE a new user
-app.post('/register/*', function (req, res) {
+app.post('/register/*', function(req, res) {
   var postBody = req.body;
   var myName = postBody.name;
   var myEmail = postBody.email;
@@ -44,13 +46,21 @@ app.post('/register/*', function (req, res) {
 
 
   pg.connect(connString, function(err, client, done) {
-    if(err) response.send("Could not connect to DB: " + err);
-    client.query('INSERT INTO users (name,email,password) VALUES ($1,$2,$3)',
-        [myName,myEmail,myPassword]);
+    if (err) response.send("Could not connect to DB: " + err);
+    client.query('SELECT exists (SELECT 1 FROM users WHERE email=($1))', [myEmail], function(moreErr, result) {
+      if (moreErr) {
+        response.send("Oops! Something went wrong. Please try again.");
+      } else if (!result.rows[0].exists) {
+        client.query('INSERT INTO users (name,email,password) VALUES ($1,$2,$3)', [myName, myEmail, myPassword]);
 
 
-    res.send('OK');
-});
+        res.send('OK');
+      } else {
+        res.send('That email has already been taken. Please try another.');
+      }
+    });
+
+  });
 
 });
 
@@ -58,30 +68,28 @@ app.post('/users/*', function(req, res) {
   var postBody = req.body;
   var myEmail = postBody.email;
   var myPassword = postBody.password;
+  client.query('SELECT exists (SELECT 1 FROM users WHERE email=($1))', [myEmail], function(moreErr, result) {
+    if (moreErr) {
+      response.send("Oops! Something went wrong. Please try again.");
+    } else if (result.rows[0].exists) {
+      client.query('SELECT * FROM users WHERE email=($1)', [myEmail], function(moreErr, result) {
 
-  client.query('SELECT * FROM users WHERE email=($1)', [myEmail], function(moreErr, result) {
-
-    if(moreErr) { response.send("There was an error: " + moreErr); }
-    if(result.rows[0].password != myPassword) {
-      res.send("Incorrect password."); }
-    else{
-    // var userInformation = [result.rows[0].name, result.rows[0].password, result.rows[0].email];
-
-    //console.log(result.rows[0]);
-    // var results = JSON.parse(result);
-    // console.log(results);
-    //console.log(res.json(result.rows[0]));
-res.send(result.rows[0]);
-
-  }
-  // }
-  // res.send("Incorrect Password.");
-
+        if (moreErr) {
+          response.send("There was an error: " + moreErr);
+        }
+        if (result.rows[0].password != myPassword) {
+          res.send("Incorrect password.");
+        } else {
+          res.send(result.rows[0]);
+        }
+      });
+    } else {
+      res.send("Invalid email. Please try again.")
+    }
   });
-
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(3000, function() {
   var port = server.address().port;
   console.log('Server started at http://localhost:%s/', port);
 });
