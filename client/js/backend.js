f5c530401a0931d50483ce5accd7469b7d39d1ff
Manan Hora <horamanan@gmail.com>
2015-11-18 17:41:47 -0500

    Added Bootstrap+ added js file

---
+++ client/js/backend.js
@@ -0,0 +1,79 @@
+var name;
+var myEmail;
+var myPassword;
+
+
+$(document).ready(function() {
+
+  //LOGIN
+  $("#logInButton").click(function() {
+    var useremail = $("#email").val();
+    var userpassword = $("#password").val();
+    $.ajax({
+      url: "users/login",
+      type: "POST",
+      dataType: "text",
+
+      data: {
+        email: $("#email").val(),
+        password: $("#password").val()
+      },
+      success: function(result) {
+        alert('Success! Welcome!');
+        console.log(result);
+        var results = JSON.parse(result);
+        var uname = results.name;
+        name = uname;
+
+        myEmail = useremail;
+        myPassword = userpassword;
+
+        $("#loggedIn").show();
+        $("#loginName").text(name);
+        $("#login").hide();
+      }
+
+    });
+  });
+
+  //UPDATE NAME
+  $("#changeNameButton").click(function() {
+    var newName = $("#updateName").val();
+
+    //Ajax call
+    $.ajax({
+      url: "users/updateName",
+      type: "POST",
+      dataType: "text",
+      data: {
+        name: newName,
+        email: myEmail,
+        password: myPassword
+      },
+      success: function(result) {
+        alert('Success! Welcome!' + result);
+        name = result;
+        $("#loginName").text(name);
+      }
+    });
+  });
+
+  //SIGN UP
+  $("#registerButton").click(function() {
+    console.log("REGSITRER");
+    $.ajax({
+      url: "register/",
+      type: "POST",
+      dataType: "text",
+      data: {
+        name: $("#name").val(),
+        email: $("#email").val(),
+        password: $("#password").val()
+      },
+      success: function(result) {
+        alert('Success! Welcome!' + result);
+      }
+    });
+  });
+
+});
