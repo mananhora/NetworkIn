d8760091e24bcf9e565cf19c76b1fdac31d083b4
Nina Bose <nbose2@u.rochester.edu>
2015-11-25 12:32:36 -0500

    Fixed Merge issue.

--- client/js/backend.js
+++ client/js/backend.js
@@ -84,7 +84,6 @@ $(document).ready(function() {
     document.cookie = 'user=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
     window.location = "index.html";
   }
-=======
 
   //ADD Member
   $("#addMember").click(function(){
